4d5fe0647fbf5cc80f768a761c0e99b65ab1e4ad
Baichik <zhumabek_uulu_b@iuca.kg>
2020-02-05 08:14:55 +0000

    add a page

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ app = Flask('app')
 def index():
   return render_template('home.html')
 
+@app.route('/update')
+def update():  
+  return render_template('update.html')
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port='8000')
+  app.run(host='0.0.0.0', port='7000')
 
 
