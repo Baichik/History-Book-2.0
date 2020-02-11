706305b2bb4c660f38e7589a8c1612abc4ca3932
Baichik <zhumabek_uulu_b@iuca.kg>
2020-02-11 10:55:58 +0000

    add new pages

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ def index():
 def update():  
   return render_template('update.html')
 
+@app.route('/download')
+def download():
+  return render_template('download.html')
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port='7000')
 
